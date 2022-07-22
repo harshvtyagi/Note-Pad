6b9130a77864ed2ae13f4c7b9d828687d5663bef
harshvtyagi <harshvardhan.2024cs1026@kiet.edu>
2022-07-23 03:37:40 +0530

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header(){
+    return <header>Notepad</header>;
+
+}
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div  className="note">
+            <h1>This is my first note</h1>
+            <p>Everyday above ground is a beautiful day!</p>
+        </div>
+    );
+}
+
+export default Note;
